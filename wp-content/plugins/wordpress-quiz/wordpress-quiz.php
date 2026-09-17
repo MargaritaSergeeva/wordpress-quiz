f4c21f1f93138343abcd813261c1c5015a6fcd81
margarita_sergeeva <margaritk@inbox.ru>
2026-09-17 13:46:51 +0300

    Use pink for quiz primary action buttons

--- wp-content/plugins/wordpress-quiz/wordpress-quiz.php
+++ wp-content/plugins/wordpress-quiz/wordpress-quiz.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WordPress Quiz
  * Description: Редактор квизов, сбор заявок и передача ответов в CRM.
- * Version: 0.2.1
+ * Version: 0.2.2
  * Requires at least: 6.8
  * Requires PHP: 8.3
  * Text Domain: wordpress-quiz
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-const VERSION = '0.2.1';
+const VERSION = '0.2.2';
 const CONSENT_VERSION = 'demo-2026-09-17';
 
 require __DIR__ . '/includes/data.php';
